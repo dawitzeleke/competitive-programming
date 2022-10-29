70b5f7c58c3f135880ebfa3ae81749d3d576c00e
dawitzeleke <109982683+dawitzeleke@users.noreply.github.com>
2022-10-28 23:25:49 -0700

    Create grading-students.py

---
+++ grading-students.py
@@ -0,0 +1,9 @@
+n = int(input().strip())
+for i in range(n):
+    grade = int(input().strip())
+
+    if grade >= 38:
+        mod5 = grade % 5
+        if mod5 >= 3:
+            grade = grade + (5 - mod5)
+    print(grade)
