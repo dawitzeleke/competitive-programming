c13570786a1ec5e091527b435aa2e02087d2b3ba
dawitzeleke <109982683+dawitzeleke@users.noreply.github.com>
2022-12-31 11:56:58 +0000

    Update fizz-buzz.py

--- fizz-buzz.py
+++ fizz-buzz.py
@@ -1,8 +1,15 @@
-class Solution:
-    def twoSum(self,list, target):
-        for i in range (len(list)):
-            for j in range (i+1, len(list)):
-                if list[i]+list[j]== target:
-                    return [i,j]
+class Solution(object):
+    def fizzBuzz(self, n):
+        answer=[]
+        for i in range (1,n+1):
+            if i%3==0 and i%5==0:
+                answer.append("FizzBuzz")
+            elif i%3==0:
+                answer.append("Fizz")
+            elif i%5==0:
+                answer.append("Buzz")
+            else:
+                answer.append(str(i))
+        return answer
                 
                 
