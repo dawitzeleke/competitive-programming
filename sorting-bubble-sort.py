6fd4784167b5557c5fc3e920c921942d52b3e58f
dawitzeleke <109982683+dawitzeleke@users.noreply.github.com>
2023-01-01 21:42:23 +0000

    Update sorting-bubble-sort.py

--- sorting-bubble-sort.py
+++ sorting-bubble-sort.py
@@ -1,10 +1,13 @@
 def countSwaps(a):
-     swaps= 0
-     for i in range(len(a)):
-         for j in range(len(a)-1):
-             if a[j]> a[j+1]:
-                 a[j],a[j+1]= a[j+1], a[j]
-                 swaps+=1
-     print("Array is sorted in " + str(swaps) + " swaps.")
-     print("First Element: "+ str(a[0]))
-     print ("Last Element: " + str(a[len(a)-1]))
+    numSwaps= 0
+    n= len(a)
+    for i in range(n):
+        j= 0
+        while j< n-1:
+            if a[j]>a[j+1]:
+                a[j],a[j+1]=a[j+1],a[j]
+                numSwaps+=1
+            j+=1
+    print("Array is sorted in" ,numSwaps, "swaps.")  
+    print("First Element:", a[0])          
+    print("Last Element:" , a[-1])
