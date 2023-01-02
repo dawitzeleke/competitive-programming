486afc57377afad7e0cd2056798ef859036c2c11
dawitzeleke <109982683+dawitzeleke@users.noreply.github.com>
2023-01-02 10:36:53 +0000

    Update selection-sort.py

--- selection-sort.py
+++ selection-sort.py
@@ -1,9 +1,23 @@
 class Solution: 
-    def selectionSort(self, arr,n):
-        for i in range(n):
-            min_index = i
-            
+    def select(self, arr, i):
+        n= len(arr)
+        for i in range(n-1):
+            selected= arr[i]
             for j in range(i+1, n):
-                if arr[j] < arr[min_index]:
-                    min_index = j
-            (arr[i], arr[min_index]) = (arr[min_index], arr[i])
+                if selected>arr[j]:
+                    selected= min(selected, arr[j])
+                else:
+                    continue
+        return selected
+    
+    def selectionSort(self, arr,n):
+        for i in range (n-1):
+            minn= i
+            for j in range(i+1,n):
+                if arr[j]<arr[minn]:
+                    minn= j
+                else:
+                    continue
+            if minn!= i:
+                arr[i], arr[minn]= arr[minn], arr[i]
+        return arr
