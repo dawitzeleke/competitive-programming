e2db66fda13fba4017c2f276c523effaccf7260b
dawitzeleke <109982683+dawitzeleke@users.noreply.github.com>
2023-01-11 19:27:24 +0000

    Runtime 110 ms ( 93.78%) Memory19.3 MB (15.44%)

--- Merge-intervals.py
+++ Merge-intervals.py
@@ -1,13 +1,12 @@
-class Solution:
+class Solution(object):
     def merge(self, intervals):
-        
-        intervals.sort(key=lambda x: (x[0]))
-        ans = []
-        ans.append(intervals[0])
-        for start_i, end_i in intervals[1:]:
-            start_j, end_j = ans[-1]
-            if start_i > end_j:
-                ans.append([start_i, end_i])
+        intervals.sort( key = lambda i: i[0])
+        result = [intervals[0]]
+        for start , end in intervals[1:]:
+            lastend = result[-1][1]
+            if lastend >= start:
+                result[-1][1] = max (lastend, end)
             else:
-              ans[-1][1] = max(end_j, end_i)
-        return ans
+                result.append([start,end])
+
+        return result
