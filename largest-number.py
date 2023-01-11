b6eae3073beb06cadfe8e72118ab93becc3a234f
dawitzeleke <109982683+dawitzeleke@users.noreply.github.com>
2023-01-11 21:25:07 +0000

    Runtime111 ms (6.51%) , Memory13.5 MB (68.49%)

--- largest-number.py
+++ largest-number.py
@@ -1,11 +1,18 @@
-class Solution(object):   
-     def largestNumber (self, nums):
-        for i, n in enumerate(nums):
-             nums [i] = str(n)
-        def compare (n1, n2):
-             if n1 + n2 > n2 + n1:
-                 return -1
-             else:
-                 return 1
-        nums = sorted(nums, key=cmp_to_key(compare))
-        return str(int("" .join(nums)))
+class Solution(object):
+    def largestNumber(self, nums):
+        n= len(nums)
+        largest_num=""
+
+        for i in range(n):
+            for j in range (i+1, n):
+                if nums[i]!=0 or nums[j]!=0:
+                    sum1= str(nums[i]) + str(nums[j])
+                    sum2= str(nums[j]) + str(nums[i])
+                    if int(sum1)< int(sum2):
+                        nums[i],nums[j]= nums[j],nums[i]
+                    else:
+                        continue
+        for k in range(n):
+            largest_num+= str(nums[k])
+        
+        return str(int(largest_num))
