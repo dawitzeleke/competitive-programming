0541ad5b8791630fbea076eb2ca3f8423bca738e
dawitzeleke <109982683+dawitzeleke@users.noreply.github.com>
2023-01-12 19:44:07 +0000

    Runtime 24 ms(58.67%), Memory13.4 MB (84.24%)

--- Valid-parentheses.py
+++ Valid-parentheses.py
@@ -1,13 +1,15 @@
 class Solution(object):
     def isValid(self, s):
-        stack=[]
-        closeToOpen= {")":"(", "]":"[", "}":"{"}
-        for c in s:
-            if c in closeToOpen:
-                if stack and stack[-1] == closeToOpen[c]:
-                    stack.pop()
+        dictionary = { "(": ")" , "[":"]" , "{":"}"}
+        stack = []
+        for p in s:
+            if p in dictionary.keys():
+                stack.append(p)
+            else: 
+                if not len(stack):
+                    return False 
                 else:
-                    return False
-            else:
-                stack.append(c)
-        return True if not stack else False
+                    x= stack.pop()
+                    if p != dictionary[x]:
+                        return False
+        return not len(stack)
