9baa9794bf3ed709946f2629a1ba3cf0b862ff50
dawitzeleke <109982683+dawitzeleke@users.noreply.github.com>
2023-03-14 16:16:27 +0000

    Time: 1714 ms (81.19%), Space: 30.1 MB (33.07%)

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -4,15 +4,11 @@ class Solution:
         deq = collections.deque()
         ans = []
         for i in range(len(nums)):
-            if i >= k-1:
-                while deq and nums[deq[-1]] < nums[i]:
+            while deq and nums[deq[-1]] < nums[i]:
                     deq.pop()
-                deq.append(i)
+            deq.append(i)
+            if i >= k-1:
                 ans.append(nums[deq[0]])
                 if nums[deq[0]] == nums[i-k+1]:
                     deq.popleft()
-            else:
-                while deq and nums[deq[-1]] < nums[i]:
-                    deq.pop()
-                deq.append(i)
         return ans
