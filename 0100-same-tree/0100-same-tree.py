cac984a724aa4f857c894a40b057a235e484d169
dawitzeleke <109982683+dawitzeleke@users.noreply.github.com>
2023-03-17 07:30:02 +0000

    Time: 27 ms (90.30%), Space: 13.7 MB (97.69%)

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -5,11 +5,12 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p and not q:
+    def helper(self, node1, node2):
+        if not node1 and not node2:
             return True
-        if not p or not q or p.val != q.val:
+        if (not node1 and node2) or (not node2 and node1) or node1.val != node2.val:
             return False
-      
-        return (self.isSameTree(p.right if p else None, q.right if q else None) and self.isSameTree(p.left if p else None, q.left if q else None))
-    
+        return self.helper(node1.left if node1 else None, node2.left if node2 else None) and self.helper(node1.right if node1 else None, node2.right if node2 else None)
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        answer = self.helper(p,q)
+        return answer
