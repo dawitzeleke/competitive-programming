c25550f7454c5a6ae96d3ac7ec4c5713f37b0f45
dawitzeleke <109982683+dawitzeleke@users.noreply.github.com>
2023-03-23 16:59:57 +0000

    Time: 1196 ms (22.41%), Space: 30.2 MB (25.38%)

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,4 +1,20 @@
 class Solution:
+    def quicksort(self, arr, start, end):
+
+        if end  - start <= 0:
+            return
+        pivot = arr[start]
+        write = start + 1
+        for read in range(start + 1, end + 1):
+            if arr[read] <= arr[start]:
+                arr[read], arr[write] = arr[write], arr[read]
+                write += 1
+        arr[start], arr[write - 1] = arr[write - 1], arr[start]
+        if len(arr) - self.k < write - 1:
+            self.quicksort(arr, start, write - 2)
+        else:
+            self.quicksort(arr, write , end)
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        nums.sort()
-        return nums[len(nums)-k]
+        self.k = k
+        self.quicksort(nums, 0, len(nums) - 1)
+        return nums[len(nums) - k]
