a4f40292d5a0ee180780e031afada21f20eca304
dawitzeleke <109982683+dawitzeleke@users.noreply.github.com>
2023-03-23 16:00:30 +0000

    Time: 163 ms (27.07%), Space: 15.3 MB (29.98%)

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,12 +1,10 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        if 0 not in nums:
-            return 0
-        else:
-            for i in range (len(nums)):
         
-                while nums[i] - 1 != i and nums[i] != 0:
-                    temp = nums[i]-1
-                    nums[i], nums[temp] = nums[temp], nums[i]
+        for i in range (len(nums)):
 
-        return nums.index(0) + 1
+            while nums[i] - 1 != i and nums[i] != 0:
+                temp = nums[i]-1
+                nums[i], nums[temp] = nums[temp], nums[i]
+
+        return nums.index(0) + 1 if 0 in nums else 0
