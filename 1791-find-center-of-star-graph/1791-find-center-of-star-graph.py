20f0148d69b0e31a98150f54977f34552be840bd
dawitzeleke <109982683+dawitzeleke@users.noreply.github.com>
2023-04-06 16:17:51 +0000

    Time: 956 ms (6.08%), Space: 58.2 MB (5.47%)

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
@@ -1,6 +1,16 @@
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
+        connection = defaultdict(list)
+        unique = set()
         for i in range(len(edges)):
-            for num in edges[i]:
-                if num in edges[i + 1]:
-                    return num
+            
+            connection[edges[i][0]].append(edges[i][1])
+            connection[edges[i][1]].append(edges[i][0])
+            unique.add(edges[i][1])
+            unique.add(edges[i][0])
+        for key, value in connection.items():
+            if len(value) == len(unique) - 1:
+                return key 
+            
+        
+            
