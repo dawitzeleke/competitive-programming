fef0e973eeb0ace08a2f494e5a0d5e27dc495065
dawitzeleke <109982683+dawitzeleke@users.noreply.github.com>
2023-04-13 19:18:39 +0300

    Time: 270 ms (63.73%), Space: 17.1 MB (71.76%)

---
+++ 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxSum(self, grid: List[List[int]]) -> int:
+        row, col = len(grid), len(grid[0])
+        max_sum = 0
+        for i in range(row - 2):
+            for j in range(col - 2):
+                hourglass = grid[i][j] + grid[i][j + 1] + grid[i][j + 2] + grid[i + 1][j + 1] + grid[i + 2][j] + grid[i + 2][j + 1] + grid[i + 2][j + 2]
+                max_sum = max(max_sum, hourglass)
+      
+        return max_sum
