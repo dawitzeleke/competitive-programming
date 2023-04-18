adf8921247eebdccee0a4505865b0d721a69cf5c
dawitzeleke <109982683+dawitzeleke@users.noreply.github.com>
2023-04-18 22:07:10 +0300

    Time: 58 ms (27.66%), Space: 17.3 MB (50.14%)

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -0,0 +1,43 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        len_cycle = 0
+        slow = head
+        fast = head
+        
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if fast == slow:
+                temp = slow
+                temp = temp.next
+                len_cycle += 1
+                while fast != temp:
+                    len_cycle += 1
+                    temp = temp.next
+                break
+        
+             
+        if len_cycle == 0:
+            return None
+        first = head
+        second = head
+        
+        while len_cycle > 0:
+            second = second.next
+            len_cycle -= 1
+        
+        while first != second:
+            first = first.next
+            second = second.next
+        print(first.val)
+        
+        return second
+       
+        
