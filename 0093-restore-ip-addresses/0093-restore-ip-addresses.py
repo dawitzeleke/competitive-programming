92ed65f55b85ad2ee73aef36637da6038f3948a7
dawitzeleke <109982683+dawitzeleke@users.noreply.github.com>
2023-04-20 13:01:16 +0300

    Time: 39 ms (53.35%), Space: 13.9 MB (23.15%)

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -0,0 +1,20 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        res = []
+        if len(s) > 12:
+            return res
+        
+        def helper(index, dots, curr):
+            if dots == 4 and index == len(s):
+                
+                res.append(curr[:-1])
+                return 
+            print(curr)
+            if dots > 4:
+                return
+            for j in range(index , min(index + 3, len(s))):
+                if int(s[index: j + 1]) < 256 and (index == j or s[index] != "0"):
+                    helper(j + 1, dots + 1, curr + s[index: j + 1] + ".")
+                    
+        helper(0, 0, "")
+        return res
