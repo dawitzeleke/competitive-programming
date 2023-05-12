8a8f55b889777cadbe58890dcd942536f5a91cd6
dawitzeleke <109982683+dawitzeleke@users.noreply.github.com>
2023-05-12 23:54:20 +0300

    Time: 52 ms (11.18%), Space: 16.3 MB (18.93%)

---
+++ 0027-remove-element/0027-remove-element.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        left = 0
+        
+        for index in range(len(nums)):
+            if nums[index] != val:
+                nums[left] = nums[index]
+                left += 1
+
+        return left
