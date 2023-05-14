5effdc3879b25bd442cae5fa3baf1ad348d4379f
dawitzeleke <109982683+dawitzeleke@users.noreply.github.com>
2023-05-14 23:43:50 +0300

    Time: 47 ms (16.16%), Space: 16.4 MB (13.84%)

---
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -0,0 +1,9 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        end = len(s) - 1
+        while end > 0 and s[end] == " ": 
+            end -= 1
+        beg = end
+        while beg >= 0 and s[beg] != " ": 
+            beg -= 1
+        return end - beg
