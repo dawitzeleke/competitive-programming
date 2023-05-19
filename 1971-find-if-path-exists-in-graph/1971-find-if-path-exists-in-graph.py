0b24aad021ae4facad40594160ab13822c811242
dawitzeleke <109982683+dawitzeleke@users.noreply.github.com>
2023-05-19 17:32:16 +0300

    Time: 2027 ms (52.56%), Space: 105.9 MB (84.10%)

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,22 +1,25 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
         
-        connections = defaultdict(list)
+        parent = {i: i for i in range(n)}
+        rank = [1 for i in range(n)]
+        def find(node):
+            if parent[node] != node:
+                parent[node] = find(parent[node])
+                
+            return parent[node]
         
-        for i in range (len(edges)):
-            connections[edges[i][0]].append(edges[i][1])
-            connections[edges[i][1]].append(edges[i][0])
+        def Union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+            if rank[p1 - 1] >= rank[p2 - 1]:
+                parent[p2] = p1
+                rank[p1 - 1] += rank[p2 - 1]
+
+            else:
+                parent[p1] = p2
+                rank[p2 - 1] += rank[p1 - 1]
             
-        def dfs(s, d, visited):
+        for a, b in edges:
+            Union(a, b)
             
-            if s == d:
-                return True
-            visited.add(s)
-            for c in connections[s]:
-                if c not in visited:
-                    found = dfs(c, d, visited)
-                    
-                    if found:
-                        return True
-                    
-        return dfs(source, destination, set())
+        return find(source) == find(destination)
