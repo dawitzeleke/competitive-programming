cebe405983aba8a53b8f0af41355fb8a3736471f
dawitzeleke <109982683+dawitzeleke@users.noreply.github.com>
2023-05-31 19:02:52 +0300

    Time: 53 ms (7.55%), Space: 16.5 MB (11.92%)

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -2,12 +2,15 @@ class Solution:
     def tribonacci(self, n: int) -> int:
         if n < 2:
             return n
-        memo = [0] * (n + 1)
-        memo[0] = 0
-        memo[1] = 1
-        memo[2] = 1
+       
+        fib1 = 0
+        fib2 = 1
+        fib3 = 1
         
         for i in range(3, n + 1):
-            memo[i] = memo[i - 1] + memo[i - 2] + memo[i - 3]
+            temp = fib1 + fib2 + fib3
+            fib1 = fib2
+            fib2 = fib3
+            fib3 = temp
             
-        return memo[n]
+        return fib3
