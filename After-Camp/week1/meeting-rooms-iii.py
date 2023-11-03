8ea742e2afedd91e717d4229ce3f55456758bdda
dawitzeleke <109982683+dawitzeleke@users.noreply.github.com>
2023-11-03 22:19:06 +0300

    Added solution for Meeting Rooms III

---
+++ After-Camp/week1/meeting-rooms-iii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
+        busy = []
+        available = [i for i in range(n)]
+        count = [0] * n
+        meetings.sort()
+        for start, end in meetings:
+            while busy and busy[0][0] <= start:
+                newEnd, room = heapq.heappop(busy)
+                heapq.heappush(available, room)
+
+            if available:
+                room = heapq.heappop(available)
+                heapq.heappush(busy, (end, room))
+
+            else:
+                time, room = heapq.heappop(busy)
+                heapq.heappush(busy, (time + end - start, room))
+
+            count[room] += 1
+        return count.index(max(count))
