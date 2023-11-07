7107667dafcfe0101ce80178437408ca06918b68
dawitzeleke <109982683+dawitzeleke@users.noreply.github.com>
2023-11-07 12:23:02 +0300

    Added solution for Isomorphic Strings

---
+++ After-Camp/week1/isomorphic-strings.py
@@ -0,0 +1,5 @@
+from collections import Counter
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        zipped_set = set(zip(s, t))
+        return len(zipped_set) == len(set(s)) == len(set(t))
