c5fabee3826260bb15c8f41f77e7271635e91508
Dawit Zeleke <109982683+dawitzeleke@users.noreply.github.com>
2025-10-08 14:00:01 +0300

    Time: 11 ms (27.85%), Space: 21.9 MB (31.1%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -3,39 +3,50 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        row, col  = len(board), len(board[0])
-        
-        
+
+        row = len(board)
+        col = len(board[0])
+        visited = set()
+
+        # is valid row and column pair
+
+        # define the directions
+        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+
+        # define the dfs function
+
         def dfs(r, c):
-            if r < 0 or c < 0 or r == row or c == col or board[r][c] != "O":
-                return 
-            
-            dirc = [[1,0], [0,1], [-1,0], [0,-1]] 
-            if board[r][c] == "O":
-                board[r][c] = "T"
-            
-            for dr, dc in dirc:
-                dfs(r + dr, c + dc)
+            if r < 0 or c < 0 or r > row - 1 or c > col - 1 or (r, c) in visited or board[r][c] != "O" :
+                return
+
+            visited.add((r, c))
+
+            for row_c, col_c in directions:
+                new_row = r + row_c
+                new_col = c + col_c
+
+                dfs(new_row, new_col)
+
         
-        for i in range(col):
-            if board[0][i] == 'O': 
-                dfs(0, i)
-            if board[row-1][i] == 'O': 
-                dfs(row-1, i)
-                
         for i in range(row):
-            if board[i][0] == 'O': 
+
+            if board[i][0] == "O":
                 dfs(i, 0)
-            if board[i][col-1] == 'O':
-                dfs(i, col-1)
-            
+            if board[i][col - 1] == "O":
+                dfs(i, col - 1)
+
+        for j in range(col):
+
+            if board[0][j] == "O":
+                dfs(0, j)
+            if board[row - 1][j] == "O":
+                dfs(row - 1, j)
+
+
         for r in range(row):
             for c in range(col):
-                if board[r][c] == "T":
-                    board[r][c] = "O"
-                elif board[r][c] == "O":
+                if board[r][c] == "O" and (r, c) not in visited:
                     board[r][c] = "X"
-                
-                    
-        return board
-        
+
+                     
+
