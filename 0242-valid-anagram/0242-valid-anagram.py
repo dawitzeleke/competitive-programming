9eed0d83ebac3c8ff580fea86466e14ed889bff1
Dawit Zeleke <109982683+dawitzeleke@users.noreply.github.com>
2025-10-11 22:01:42 +0300

    Time: 12 ms (52.01%), Space: 17.8 MB (86.37%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,24 +1,20 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        fre1 = {}
-        fre2 ={}
+
+        t_count = defaultdict(int)
+        s_count = defaultdict(int)
         if len(s) != len(t):
             return False
-        for i in s:
-            if i in fre1:
-                fre1[i] +=1
-            else:
-                fre1[i] = 1
-        for i in t:
-            if i in fre2:
-                fre2[i] +=1
-            else:
-                fre2[i] = 1
-        for c in t:
-            if c in fre1 and fre2[c] == fre1[c]:
-                continue
-            else:
+
+        for char in t:
+            t_count[char] += 1
+
+        for char in s:
+            s_count[char] += 1
+
+
+        for char, count in t_count.items():
+            if s_count[char] != count:
                 return False
-                
-        return True
-            
+
+        return True 
