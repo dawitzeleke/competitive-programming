e443e21418c1b193941f21b071cf06609297d155
Dawit Zeleke <109982683+dawitzeleke@users.noreply.github.com>
2025-10-12 16:37:45 +0300

    Time: 253 ms (47.01%), Space: 28.4 MB (10.2%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,29 +1,29 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
+
         row = len(grid)
         col = len(grid[0])
-        answer = 0 
+        directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+        
         visited = set()
-        def bfs(r,c):
-            queue = collections.deque()
-            visited.add((r,c))
-            queue.append((r,c))
-            diff = [[0,1], [1,0], [-1, 0], [0,-1]]
-            while queue:
-                ro, co = queue.popleft()
-                for dr, dc in diff:
-                    rn = ro + dr
-                    cn = co + dc
-                    if rn in range(row) and cn in range(col) and grid[rn][cn] == "1" and (rn, cn) not in visited:
-                        visited.add((rn, cn))
-                        queue.append((rn, cn))
-                
+
+        def dfs(r, c):
+            if r < 0 or r >= row or c < 0 or c >= col or grid[r][c] != "1":
+                return
+
+            visited.add((r, c))
+
+            for dr, dc in directions:
+                new_r = r + dr
+                new_c = c + dc
+                if (new_r, new_c) not in visited:
+                    dfs(new_r, new_c)
+        answer = 0
+        
         for r in range(row):
             for c in range(col):
                 if grid[r][c] == "1" and (r, c) not in visited:
-                    bfs(r,c)
-                    
+                    dfs(r, c)
                     answer += 1
+
         return answer
