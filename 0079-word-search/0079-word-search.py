2eabaa509ab43079cfa860abae8b9e212e50934e
Dawit Zeleke <109982683+dawitzeleke@users.noreply.github.com>
2025-10-13 14:28:13 +0300

    Time: 6702 ms (7.49%), Space: 17.9 MB (39.49%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,20 +1,31 @@
 class Solution:
-    
     def exist(self, board: List[List[str]], word: str) -> bool:
-        rows, cols = len(board), len(board[0])
+        row = len(board)
+        col = len(board[0])
+
+        directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
         path = set()
-        
-        def helper(r, c, i):
+        def isValidGrid(r, c):
+            return r >= 0 and r < row and c >= 0 and c < col
+
+        def dfs(r, c, i):
+
             if i == len(word):
                 return True
-            if r < 0 or c < 0 or r >= rows or c >= cols or word[i] != board[r][c] or (r,c) in path:
+
+            if  not isValidGrid(r, c) or board[r][c] != word[i] or (r, c) in path:
                 return False
-            path.add((r,c))
-            result = helper(r+1 ,c , i+1) or helper(r ,c+1 , i+1) or helper(r-1 ,c , i+1) or helper(r ,c - 1 , i+1)
-            path.remove((r,c))
-            return result
-        for i in range(rows):
-            for j in range(cols):
-                if helper(i, j, 0):
+            path.add((r, c))
+            res = False
+            for dr, dc in directions:
+                new_r = r + dr
+                new_c = c + dc
+
+                res = res or dfs(new_r, new_c, i + 1)
+            path.remove((r, c))
+            return res
+        for r in range(row):
+            for c in range(col):
+                if dfs(r, c, 0):
                     return True
-        return False      
+        return False
